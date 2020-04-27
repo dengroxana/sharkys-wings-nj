9be1a51c62210f86c3f275fa2b9606d2ed2d40f6
Myra <myrakornides@gmail.com>
2020-04-27 15:37:28 -0400

    happy hour days and menu done √

--- sharkys-wings-nj/src/App.jsx
+++ sharkys-wings-nj/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
 import BarMenu from "./components/BarMenu"
+import HappyHour from "./components/HappyHour"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<Header/>
-<BarMenu/>
+      <Header />
+      <HappyHour />
+      <BarMenu />
     </div>
   );
 }
