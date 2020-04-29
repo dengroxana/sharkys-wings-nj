75ab67624c98ae5977e77296a2fe596d671076d7
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2020-04-29 16:02:03 -0400

    adding items

--- sharkys-wings-nj/src/components/Wings.jsx
+++ sharkys-wings-nj/src/components/Wings.jsx
@@ -7,7 +7,7 @@ function Wings() {
   return (
     <div className="menuList">
  <ul className="leaders">
- <li><span>10 piece</span> <span>$11.99</span></li>
+ <li>10 piece<span></span>$11.99</li>
  <li><span>16 piece</span> <span>$15.99</span> </li>
  <li><span>25 piece</span> <span>$24.99</span> </li>
  <li><span>50 piece</span> <span>$48.99</span> </li>
