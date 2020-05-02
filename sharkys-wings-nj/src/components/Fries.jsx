9d88ee03d7abf8ba5e8436801cf492d3ededc7c4
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2020-05-02 17:27:23 -0400

    added all menu items

--- sharkys-wings-nj/src/components/Fries.jsx
+++ sharkys-wings-nj/src/components/Fries.jsx
@@ -3,7 +3,50 @@ import React from 'react';
 function Fries() {
     return(
         <div class="menuList">
-            <p>Fries. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .$5.99</p>
+            <div className="wing-container">
+        <div className="col">Sharky Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$9.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Scampi Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$9.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Cheese Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$6.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Maryland Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$9.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Buffalo Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$8.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Nacho Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$9.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Country Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$9.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Truffle Parmesan Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$10.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Steak Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$6.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Curly Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$6.99</div>
+      </div>
+      <div className="wing-container">
+        <div className="col">Sweet Potato Fries</div> <div className="dots"></div>{" "}
+        <div className="col">$7.99</div>
+      </div>
         </div>
     )
 }
